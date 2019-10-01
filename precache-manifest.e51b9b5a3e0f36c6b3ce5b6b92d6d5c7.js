8c478068403b17690fdd46b78d834c20278a0d94
abhishe3571 <abhishek3571@gmail.com>
2019-10-02 05:21:57 +0530

    Updates

---
+++ precache-manifest.e51b9b5a3e0f36c6b3ce5b6b92d6d5c7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b011a494d8f675c0e99edb075ffbbf4",
+    "url": "/crwn-clothing/index.html"
+  },
+  {
+    "revision": "03d1aa810d882e66dc43",
+    "url": "/crwn-clothing/static/css/main.8ee10945.chunk.css"
+  },
+  {
+    "revision": "e5f91000a52d84f9e4ee",
+    "url": "/crwn-clothing/static/js/2.1a63e494.chunk.js"
+  },
+  {
+    "revision": "03d1aa810d882e66dc43",
+    "url": "/crwn-clothing/static/js/main.2ce3e035.chunk.js"
+  },
+  {
+    "revision": "4d332182b32d819ff6f4",
+    "url": "/crwn-clothing/static/js/runtime-main.7afac7a2.js"
+  }
+]);
