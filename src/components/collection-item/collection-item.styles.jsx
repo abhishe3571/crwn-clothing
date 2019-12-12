cd165419f1f1278f07fd9ab5ed80d01bf3e5cc8a
abhishe3571 <abhishek3571@gmail.com>
2019-12-13 04:26:32 +0530

    Add styles for the mobile support

--- src/components/collection-item/collection-item.styles.jsx
+++ src/components/collection-item/collection-item.styles.jsx
@@ -2,31 +2,58 @@ import styled from 'styled-components';
 import CustomButton from '../custom-button/custom-button.component';
 
 export const CollectionItemContainer = styled.div`
-  width: 22vw;
-  display: flex;
-  flex-direction: column;
-  height: 350px;
-  align-items: center;
-  position: relative;
+    width: 22vw;
+    display: flex;
+    flex-direction: column;
+    height: 350px;
+    align-items: center;
+    position: relative;
 
-  &:hover {
-    .image {
-      opacity: 0.8;
+    &:hover {
+        .image {
+            opacity: 0.8;
+        }
+
+        button {
+            opacity: 0.85;
+            display: flex;
+        }
     }
 
-    button {
-      opacity: 0.85;
-      display: flex;
-    }
-  }
+    @media screen and (max-width: 800px) {
+        width: 40vw;
+        &:hover {
+            .image {
+                opacity: unset;
+            }
+    
+            button {
+                opacity: unset;            }
+        }
+}
+
+
 `;
 
 export const AddButton = styled(CustomButton)`
-  width: 80%;
-  opacity: 0.7;
-  position: absolute;
-  top: 255px;
-  display: none;
+    width: 80%;
+    opacity: 0.7;
+    position: absolute;
+    top: 255px;
+    display: none;
+
+    @media screen and (max-width: 800px) {
+        display: block;
+        opacity: 0.9;
+        min-width: unset;
+        padding: 0 10px;
+    }
+
+
+
+
+
+
 `;
 
 export const BackgroundImage = styled.div`
